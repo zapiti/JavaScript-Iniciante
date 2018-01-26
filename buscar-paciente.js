3686bbb21de7351ebbd0790d8e493c829d0ddb0c
nathan ranghel <31676810+zapiti@users.noreply.github.com>
2018-01-26 15:03:04 -0200

    Add files via upload

---
+++ buscar-paciente.js
@@ -0,0 +1,37 @@
+
+var btnAdicionar = document.querySelector("#buscar-pacientes");
+
+
+btnAdicionar.addEventListener("click", function(){
+
+    var xhr = new XMLHttpRequest();
+
+      xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load",function () {
+
+      if(xhr.status == 200){
+            var erroAjax = document.querySelector("#erro-ajax");
+              erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+      }else{
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        var erroAjax = document.querySelector("#erro-ajax");
+        erroAjax.classList.remove("invisivel");
+      }
+
+
+
+
+    });
+
+
+xhr.send();
+
+});
---
+++ remover-paciente.js
@@ -0,0 +1,29 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+
+
+            if (event.target.tagName == 'TD') {
+              event.target.parentNode.classList.add("fadeOut");
+
+             setTimeout(function() {
+                 event.target.parentNode.remove();
+             }, 800);
+          }
+
+
+          
+
+});
+
+
+//
+// pacientes.forEach(function(paciente) {
+//   paciente.addEventListener("dblclick",function(event){
+//      event.target.parentNode.remove();
+//   });
+//
+// });
